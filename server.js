54d67daefb5084fe49cb9e83acb50576074ba0df
Ballard Ingram <ballardingram@outlook.com>
2022-04-19 15:44:12 -0500

    12.2.4 - build database save point

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
+const mysql = require('mysql2');
 const express = require('express');
 
-const PORT  = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Express Middleware
-app.use(express.urlencoded({extended: false}));
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Default response for any other request (NOT FOUND)
-app.use ((req,res) => {
-    res.status(404).end();
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'B#nTuck#r1989',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
 });
 
-app.listen(PORT,() => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
