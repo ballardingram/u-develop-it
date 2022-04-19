d2668d655211ef61707ad41684c89966b75673e7
Ballard Ingram <ballardingram@outlook.com>
2022-04-19 15:55:42 -0500

    12.2.4 - save point - end of section

--- server.js
+++ server.js
@@ -21,9 +21,36 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
   );
 
-  db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-  });
+  // FIND > ALL CANDIDATES
+  //db.query(`SELECT * FROM candidates`, (err, rows) => {
+    //console.log(rows);
+  //});
+
+  // FIND > SPECIFIC CANDIDATE
+ // db.query(`SELECT * FROM candidates WHERE id = 1`, (err, rows) => {
+   // console.log(rows);
+  //});
+
+// DELETE A CANDIDATE
+//db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err,result) => {
+  //  if(err) {
+    //    console.log(err);
+    //}
+    //console.log(result);
+//});
+
+// CREATE A CANDIDATE
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+VALUES (?,?,?,?)`;
+ 
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
